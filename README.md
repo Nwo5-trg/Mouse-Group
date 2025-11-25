7d54a4abdc203ed5761936fd7f6837896db17b93
Fei <cubecereal@gmail.com>
2025-11-25 18:55:56 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Mouse Group
+utilities 4r fw the mouse in lvls
+
+## Stuffs
+- have a group follow the mouse
+- save mouse pos in item ids
+- force show mouse
+
+## Config
+everything in the mod is configured w/ a *special txt obj* that starts w/ `MGC!`
+
+now to configure the groups and items we use 4r stuffs the syntax is `[ARG][SEPERATOR][VAL]` (and js `[ARG]` 4r bools)
+
+all valid args r
+
+- `FG` (sets gid that follows mouse, by default takes the center of all objs in the group and folllows around that but if a group parent is specified that gets used | takes number)
+- `IF` (sets item id that controls if prev mentioned gid follows mouse, if this item id is set to 0 then the gid wont follow mouse | takes number)
+- `XI/YI` (sets item id that gets set to ***screen*** mouse x/y | takes number)
+- `XGI/YGI` (sets item id that gets set to ***game*** (wat the gid thats following the mouse gets moved to) mouse x/y | takes number)
+- `FMV` (forces the mouse to always b visible | takes nothing)
+
+everything that isnt the arg + char and its following val is whitespace so ur txt could b `MGC!FG[6IF[2FMV` or have pretty formatting like `MGC! owo FG=5; IF=2; FMV; >w<`
+
+## Credits
+- ***NicksNews*** 4r the mod suggestion :3
+
+## Rawr
+first time making a logo in photopea hope it turned out just beautiful as always :3
---
+++ src/main.cpp
@@ -0,0 +1,175 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/GJBaseGameLayer.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+#include <Geode/modify/LevelEditorLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(GJBaseGameLayerHook, GJBaseGameLayer) {
+    struct Fields {
+        bool containsConfigObj;
+        bool isPlaying;
+
+        bool forceMouseVisible;
+
+        int followGroupID;
+        int itemFlagID;
+        int xItemID;
+        int yItemID;
+        int xGameItemID;
+        int yGameItemID;
+
+        CCPoint lastMousePos;
+    };
+
+    TextGameObject* findSettingsObj() {
+        for (auto obj : CCArrayExt<GameObject*>(m_objects)) {
+            if (obj->m_objectID != 914) continue;
+
+            auto textObj = static_cast<TextGameObject*>(obj);
+            if (std::string(textObj->m_text).starts_with("MGC!")) return textObj;
+        }
+        return nullptr;
+    }
+
+    void findArg(std::string_view pStr, const char* pArg, int& pVar) {
+        const auto i = pStr.find(pArg);
+        const auto val = i + strlen(pArg) + 1;
+        pVar = i == std::string::npos || val >= pStr.size() ?
+            0 : numFromString<int>(pStr.substr(val)).unwrapOrDefault();
+    }
+    void findArg(std::string_view pStr, const char* pArg, bool& pVar) {
+        const auto i = pStr.find(pArg);
+        pVar = i != std::string::npos;
+    }
+    void parseSettingsObj(TextGameObject* pObj) {
+        auto fields = m_fields.self();
+
+        fields->containsConfigObj = pObj;
+
+        const auto str = pObj  ? std::string(pObj->m_text) : std::string();
+
+        findArg(str, "FG", fields->followGroupID);
+        findArg(str, "IF", fields->itemFlagID);
+        findArg(str, "XI", fields->xItemID);
+        findArg(str, "YI", fields->yItemID);
+        findArg(str, "XGI", fields->xGameItemID);
+        findArg(str, "YGI", fields->yGameItemID);
+        findArg(str, "FMV", fields->forceMouseVisible);
+    }
+
+    // borrowed from eclipse cuz of preveters comment like 6 months ago
+    CCPoint getGameMousePos() {
+        const auto mousePos = cocos::getMousePos();
+
+        const auto angle = m_gameState.m_cameraAngle / 0.01745328;
+        const auto rotatedPos = ccp(
+            mousePos.x * cosf(angle) - mousePos.y * sinf(angle),
+            mousePos.x * sinf(angle) + mousePos.y * cosf(angle)
+        );
+
+        return m_gameState.m_cameraPosition + rotatedPos / m_gameState.m_cameraZoom;
+    }
+
+    void setItem(int pID, int pVal) {
+        if (!pID) return;
+
+        m_effectManager->updateCountForItem(pID, pVal);
+        updateCounters(pID, pVal);
+    }
+
+    void updateMouseObjs() {
+        auto fields = m_fields.self();
+        if (!fields->followGroupID || !m_effectManager->countForItem(fields->itemFlagID)) return;
+
+        CCPoint center;
+        auto objs = static_cast<CCArray*>(getGroup(fields->followGroupID));
+        if (!objs->count()) return;
+
+        auto parent = getGroupParent(fields->followGroupID);
+        if (parent && objs->containsObject(parent)) {
+            center = parent->getPosition();
+        }
+        else if (objs->count() == 1) {
+            center = static_cast<GameObject*>(objs->firstObject())->getPosition();
+        }
+        else {
+            CCPoint max = {-FLT_MAX, -FLT_MAX}, min = {FLT_MAX, FLT_MAX};
+            for (auto obj : CCArrayExt<GameObject*>(objs)) {
+                const auto pos = obj->getPosition();
+                if (max.x < pos.x) max.x = pos.x;
+                if (max.y < pos.y) max.y = pos.y;
+                if (min.x > pos.x) min.x = pos.x;
+                if (min.y > pos.y) min.y = pos.y;
+            }
+            center = (min + max) / 2;
+        }
+
+        const auto mousePos = getGameMousePos();
+        moveObjects(objs, mousePos.x - center.x, mousePos.y - center.y, false);
+    }
+
+    void updateMousePos() {
+        auto fields = m_fields.self();
+
+        if (fields->xItemID || fields->yItemID) {
+            const auto pos = cocos::getMousePos();
+            setItem(fields->xItemID, pos.x);
+            setItem(fields->yItemID, pos.y);
+        }
+
+        if (fields->xGameItemID || fields->yGameItemID) {
+            const auto pos = getGameMousePos();
+            setItem(fields->xGameItemID, pos.x);
+            setItem(fields->yGameItemID, pos.y);
+        }
+    }
+
+
+
+
+    
+    bool init() {
+        if (!GJBaseGameLayer::init()) return false;
+        // 0s out everything
+        parseSettingsObj(nullptr);
+        m_fields->isPlaying = false;
+        return true;
+    }
+
+    void update(float p0) {
+        GJBaseGameLayer::update(p0);
+        auto fields = m_fields.self();
+
+        if (!fields->containsConfigObj || !fields->isPlaying) return;
+
+        updateMouseObjs();
+        updateMousePos();
+        if (fields->forceMouseVisible) PlatformToolbox::showCursor();
+    }
+};
+
+class $modify(PlayLayer) {
+    void setupHasCompleted() {
+        PlayLayer::setupHasCompleted();
+
+        auto gjbgl = reinterpret_cast<GJBaseGameLayerHook*>(this);
+        gjbgl->parseSettingsObj(gjbgl->findSettingsObj());
+        gjbgl->m_fields->isPlaying = true;
+    }
+};
+
+class $modify(LevelEditorLayer) {
+    void onPlaytest() {
+        auto gjbgl = reinterpret_cast<GJBaseGameLayerHook*>(this);
+        gjbgl->parseSettingsObj(gjbgl->findSettingsObj());
+        gjbgl->m_fields->isPlaying = true;
+
+        LevelEditorLayer::onPlaytest();
+    }
+
+    void onStopPlaytest() {
+        reinterpret_cast<GJBaseGameLayerHook*>(this)->m_fields->isPlaying = false;
+        LevelEditorLayer::onStopPlaytest();
+    }
+};
